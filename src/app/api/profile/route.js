5b8abc2d4c55741491c813b24e281e858d38258f
brenno luan <brennoluan8530@gmail.com>
2026-06-17 19:46:40 -0300

    feat: enhance avatar upload process by adding deletion of old images and integrating database user checks

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,18 +1,30 @@
-import { put } from '@vercel/blob';
+import { put, del } from '@vercel/blob';
 import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { options } from '../auth/[...nextauth]/options';
+import db from '../../../../prisma/db';
 
 export async function POST(request) {
   const session = await getServerSession(options);
 
   const filename = session.user.name;
 
+  const dbuser = await db.user.findFirst({
+    where: {
+      email: user.email,
+    },
+  });
+
+  if (dbuser.avatar) {
+    console.log('Vamos deletar a imagem antiga:', dbuser.avatar);
+    await del(dbuser.image);
+  }
+
   const blob = await put(filename, request.body, {
     access: 'public',
   });
 
-  db.user.update({
+  await db.user.update({
     where: {
       email: session.user.email,
     },
