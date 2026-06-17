a90804fce76af6a3e19e915b00de6a66158b21e2
brenno luan <brennoluan8530@gmail.com>
2026-06-17 17:58:07 -0300

    feat: enhance Button component with outline style and link support

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,18 @@
-import styles from './button.module.css'
+import Link from 'next/link';
+import styles from './button.module.css';
 
-export const Button = ({children}) => {
-    return <button className={styles.btn}>
+export const Button = ({ children, outline, href }) => {
+  if (href) {
+    return (
+      <Link href={href} className={outline ? styles.outline : styles.btn}>
         {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={outline ? styles.outline : styles.btn}>
+      {children}
     </button>
-}
+  );
+};
