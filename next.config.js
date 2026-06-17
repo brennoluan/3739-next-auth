af787bd1ad35bcc8ad19d7354814a6bf434a19be
brenno luan <brennoluan8530@gmail.com>
2026-06-17 18:33:53 -0300

    fix: update image remote patterns in next.config.js to allow images from all GitHub user content domains

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        // domains: [
-        //     'raw.githubusercontent.com'
-        // ],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                port: '',
-                pathname: '**',
-            },
-        ],
-    }
-}
+  images: {
+    // domains: [
+    //     'raw.githubusercontent.com'
+    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.githubusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
