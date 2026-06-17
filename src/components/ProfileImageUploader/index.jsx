040f2aac47b7f7e587a9a6a7232d6ce2cb16dd73
brenno luan <brennoluan8530@gmail.com>
2026-06-17 19:45:30 -0300

    feat: implement ProfileImageUploader component for user avatar management

---
+++ src/components/ProfileImageUploader/index.jsx
@@ -0,0 +1,61 @@
+'use client';
+import { useState } from 'react';
+import { Button } from '../Button';
+import avatarDefault from './empty-avatar.png';
+import Image from 'next/image';
+
+export const ProfileImageUploader = ({ user }) => {
+  const [imgSrc, setImgSrc] = useState(
+    user.avatar ?? user.image ?? avatarDefault,
+  );
+  const [newAvatar, setNewAvatar] = useState(null);
+
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+      setNewAvatar(file);
+
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setImgSrc(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function uploadAvatar(event) {
+    event.preventDefault();
+    fetch('/api/profile', {
+      method: 'POST',
+      body: newAvatar,
+    });
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <>
+      <ul>
+        <li>{user.name}</li>
+        <li>
+          <Image
+            src={imgSrc}
+            width={254}
+            height={254}
+            alt={`Avatar do(a) ${user.name}`}
+          />
+        </li>
+      </ul>
+      <form onSubmit={uploadAvatar}>
+        <input type="file" onChange={handleFileChange} required />
+
+        <Button>Upload</Button>
+      </form>
+    </>
+  );
+};
