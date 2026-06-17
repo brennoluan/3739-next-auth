2bb31376c0d8a3f0717e0d9f7542f322e8fd7cc3
brenno luan <brennoluan8530@gmail.com>
2026-06-17 19:58:17 -0300

    feat: update ThumbsUpButton to include session validation and disable button when not authenticated

--- src/components/CardPost/ThumbsUpButton.jsx
+++ src/components/CardPost/ThumbsUpButton.jsx
@@ -1,16 +1,19 @@
-'use client'
+'use client';
 
-import { IconButton } from "../IconButton"
-import { Spinner } from "../Spinner"
-import { ThumbsUp } from "../icons/ThumbsUp"
+import { useSession } from 'next-auth/react';
+import { IconButton } from '../IconButton';
+import { Spinner } from '../Spinner';
+import { ThumbsUp } from '../icons/ThumbsUp';
 
-import { useFormStatus } from 'react-dom'
+import { useFormStatus } from 'react-dom';
 
 export const ThumbsUpButton = () => {
-    const { pending } = useFormStatus()
-    return (
-        <IconButton disabled={pending}>
-            { pending ? <Spinner /> : <ThumbsUp /> }
-        </IconButton>
-    )
-}
+  const { pending } = useFormStatus();
+  const { data: session } = useSession();
+
+  return (
+    <IconButton disabled={!session || pending}>
+      {pending ? <Spinner /> : <ThumbsUp />}
+    </IconButton>
+  );
+};
